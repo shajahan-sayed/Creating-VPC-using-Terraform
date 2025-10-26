e0e405ca80d02ce17ea2ed2f7b75aacd33123b8f
shajahan-sayed <shajahansayed111@gmail.com>
2025-10-26 18:49:36 +0530

    Update vpc-main.tf

--- vpc-main.tf
+++ vpc-main.tf
@@ -38,6 +38,98 @@ resource "aws_subnet" "private" {
     }
     depends_on = [aws_internet_gateway.vpc-igw]
   }
-  
-  
-  
+#Route table for public 
+resource "aws_route_table" "public_route" {
+  vpc_id = aws_vpc.vpc1.id
+  tags = {
+    Name = "public_route"
+  }
+}
+#Route for IGW
+resource "aws_route" "public_internet_access" {
+  route_table_id = aws_route_table.public_route.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.vpc-igw.id
+}
+# Associate Public Route Table
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_route.id
+}
+
+# Private Route Table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc1.id
+  tags = {
+    Name = "private_route"
+  }
+}
+# Route for NAT Gateway
+resource "aws_route" "private_nat_route" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.vpc-nat.id
+}
+# Associate Private Route Table
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
+}
+#creating security group
+resource "aws_security_group" "vpc-sg" {
+   name        = "vpc-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_vpc.vpc1.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "vpc-sg"
+  }
+}
+
+# EC2 Instance - Public
+resource "aws_instance" "public_instance" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.vpc-sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "public-instance"
+  }
+}
+
+# EC2 Instance - Private
+resource "aws_instance" "private_instance" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.private.id
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.vpc-sg.id]
+
+  tags = {
+    Name = "private-instance"
+  }
+}
